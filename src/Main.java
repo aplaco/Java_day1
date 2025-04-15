24fcbc44663ec083f3d98362f576e3a5f03b2bce
aplaco <lyon108@naver.com>
2025-04-15 11:49:29 +0900

    Map

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
+import java.util. *;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("점수를 입력하세요: ");
-        int score = scanner.nextInt();
+        //자바에서의 map: 자바스크립트에서의 객체와 비슷
+        //value값에 해당하는 자료형이 다양하게 들어올떄는 Object로 타입 지정
+        Map<String, Object> student1 = new HashMap<>();
+        student1.put("name","David");
+        student1.put("age",20);
+        student1.put("isFemal", false);
 
-        if(score >= 90) {
-            System.out.println("A");
-        }
-
-        else if (score >= 85) {
-            System.out.println("B");
-        }
-
-        else {
-            System.out.println("C이하");
-        }
+        System.out.println(student1);
     }
 }
